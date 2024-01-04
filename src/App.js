5d3fe5a4f72503bfb28330e845bec9391f2f4d5e
Muminjon <juramirzaev.muminjon@gmail.com>
2024-01-04 22:12:38 +0100

    rednering dynamicly while searching

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard.tsx";
+
 //bbbbbb27
 const API_URL = "https://www.omdbapi.com?apikey=bbbbbb27";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('')
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data.Search);
+    setMovies(data.Search);
   };
 
   useEffect(() => {
     searchMovies();
   }, []);
-  return <></>;
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
+      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
